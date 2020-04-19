3e70b1d895a20e79da5cdb4d528fedfee0efe90a
starforever <starforever00@gmail.com>
2020-04-18 20:37:16 -0700

    Break long command into multiple lines

--- clean.sh
+++ clean.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
 black ${@}
-autoflake --in-place --remove-all-unused-imports --remove-unused-variables ${@}
+
+autoflake \
+  --in-place \
+  --remove-all-unused-imports \
+  --remove-unused-variables \
+  ${@}
+
 isort ${@}
