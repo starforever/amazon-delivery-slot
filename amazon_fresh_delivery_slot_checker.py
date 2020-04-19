d3623fc8e2e0ef259340d7693a1e3af8c228a544
starforever <starforever00@gmail.com>
2020-04-18 19:12:29 -0700

    Reformat with black

--- amazon_fresh_delivery_slot_checker.py
+++ amazon_fresh_delivery_slot_checker.py
@@ -8,35 +8,35 @@ import os
 
 
 def getWFSlot(productUrl):
-   headers = {
-       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-   }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+    }
 
-   driver = webdriver.Chrome()
-   driver.get(productUrl)
-   html = driver.page_source
-   soup = bs4.BeautifulSoup(html)
-   time.sleep(60)
-   no_open_slots = True
+    driver = webdriver.Chrome()
+    driver.get(productUrl)
+    html = driver.page_source
+    soup = bs4.BeautifulSoup(html)
+    time.sleep(60)
+    no_open_slots = True
 
-   while no_open_slots:
-      driver.refresh()
-      print("refreshed")
-      html = driver.page_source
-      soup = bs4.BeautifulSoup(html)
-      time.sleep(2)
+    while no_open_slots:
+        driver.refresh()
+        print("refreshed")
+        html = driver.page_source
+        soup = bs4.BeautifulSoup(html)
+        time.sleep(2)
 
-      try:
-         open_slots = soup.find('div', class_ ='orderSlotExists').text()
-         if open_slots != "false":
-            print('SLOTS OPEN!')
-            os.system('say "Slots for delivery opened!"')
-            no_open_slots = False
-            time.sleep(1400)
-      except AttributeError:
-         continue
-
-
-getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
+        try:
+            open_slots = soup.find("div", class_="orderSlotExists").text()
+            if open_slots != "false":
+                print("SLOTS OPEN!")
+                os.system('say "Slots for delivery opened!"')
+                no_open_slots = False
+                time.sleep(1400)
+        except AttributeError:
+            continue
 
 
+getWFSlot(
+    "https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1"
+)
